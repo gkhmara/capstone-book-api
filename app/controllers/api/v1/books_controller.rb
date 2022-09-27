1edf63705c8583eec63ad96268328a5e858deded
Greg Khmara <gkhmara@gmail.com>
2022-09-26 20:51:14 -0700

    update to CRUD

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class BooksController < ApplicationController
+      ALLOWED_DATA = %[title author rating].freeze
 
       def index
         books = Book.all
@@ -10,6 +11,21 @@ module Api
       def show
         book = Book.find(params[:id])
         render json: book
+      end
+
+      def create
+        data = json_payload.select {|k| ALLOWED_DATA.include?(k)}
+        book = Book.new(data)
+        if book.save
+          render json: book
+        else
+          render json: {"error": "Sorry. We could not add the book."}
+        end
+      end
+
+      def destroy
+        book = Book.find(params[:id])
+        book.destroy
       end
 
     end
